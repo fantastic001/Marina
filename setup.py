2ea9fd67ba3a12e0affc2ad9299cfca7eac15e56
Stefan Nožinić <stefan@lugons.org>
2017-02-04 15:38:44 +0100

    Release 0.12

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'Marina-decide',
   packages = ['Marina-decide'], # this must be the same as the name above
-  version = '0.10',
+  version = '0.12',
   description = 'Decision making tool',
+  package_dir = {'Marina-decide': 'lib'},
   author = 'Stefan Nožinić',
   author_email = 'stefan@lugons.org',
   url = 'https://github.com/fantastic001/Marina', # use the URL to the github repo
-  download_url = 'https://github.com/fantastic001/Marina/tarball/0.10', 
+  download_url = 'https://github.com/fantastic001/Marina/tarball/0.12', 
   keywords = ['decision-making', 'scoring', 'organization', 'lifehack'], # arbitrary keywords
   classifiers = [],
-  scripts = ["Marina-decide/marina-build", "Marina-decide/marina-decide"],
-  install_requires=[] # dependencies listed here 
+  scripts = ["marina-build", "marina-decide"],
 )
